949935d4a57199f1eaf3912d654f508722989ce2
adiyasa <adiyasanauval@gmail.com>
2015-11-02 16:50:48 +0700

    persistence class for domain and repository

---
+++ src/main/java/com/pickle/persistence/domain/Account.java
@@ -0,0 +1,7 @@
+package com.pickle.persistence.domain;
+
+/**
+ * Created by admin on 11/2/2015.
+ */
+public class Account {
+}
---
+++ src/main/java/com/pickle/persistence/repository/AccountRespository.java
@@ -0,0 +1,7 @@
+package com.pickle.persistence.repository;
+
+/**
+ * Created by admin on 11/2/2015.
+ */
+public class AccountRespository {
+}
