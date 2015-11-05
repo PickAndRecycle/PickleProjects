9f83a13a6395e9e3c0f3fc114eb0807daed4b8e5
adiyasa <adiyasanauval@gmail.com>
2015-11-05 17:06:31 +0700

    revert to last yanuar commit

--- src/main/java/com/pickle/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.pickle.service;
-
-import com.pickle.vo.AccountVO;
-
-/**
- * Created by kamar on 4/11/2015.
- */
-public interface AccountService extends BaseService<AccountVO, String> {
-}
--- src/main/java/com/pickle/service/TrashService.java
+++
@@ -1,9 +0,0 @@
-package com.pickle.service;
-
-import com.pickle.vo.TrashVO;
-
-/**
- * Created by Yanuar Wicaksana on 11/2/15.
- */
-public interface TrashService extends BaseService<TrashVO, String> {
-}
