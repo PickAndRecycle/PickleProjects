98bdd8befb9f0fb2c3074fec19b32699fdeeeddb
yanuarwicaksana <yanuwicaksana@gmail.com>
2015-11-29 22:35:23 +0700

    Test Unit for Trash Services (Please change the Latitude&Longitude attributes in trash into int first)

--- src/test/java/com/pickle/service/impl/TrashServiceImplTest.java
+++ src/test/java/com/pickle/service/impl/TrashServiceImplTest.java
@@ -1,39 +1,46 @@
 package com.pickle.service.impl;
 
-import com.pickle.persistence.repository.TrashRepository;
-import com.pickle.rest.TrashController;
+
+import com.pickle.persistence.domain.Trash;
 import com.pickle.service.TrashService;
 import com.pickle.vo.TrashVO;
 import org.junit.Test;
-import org.mockito.Mock;
-import static org.mockito.Mockito.*;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.math.BigDecimal;
 import java.util.Collection;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext-test.xml")
 public class TrashServiceImplTest {
 
     @Autowired
     private TrashService trashService;
 
-    @Autowired
-    private TrashRepository trashRepository;
-
-    @Autowired
-    private TrashController trashController;
-
-
-
     @Test
     public void testAdd() throws Exception {
-        TrashRepository mockTrashRepository = mock(TrashRepository.class);
-        trashService = new TrashServiceImpl(mockTrashRepository);
+
         TrashVO trashVO = new TrashVO();
         trashVO.setId("123");
+        trashVO.setThumbnailUrl("");
+        trashVO.setCategories("Unused Goods");
+        trashVO.setTrash_condition("Good");
+        trashVO.setUsername("Yanuar");
+        trashVO.setTitle("Test Trash");
+        trashVO.setStatus(0);
+        trashVO.setDescription("Test Description");
+        trashVO.setDistance(10);
+        trashVO.setLatitude(20);
+        trashVO.setLongitude(20);
+        trashVO.setPhoto_url("");
+        trashVO.setSize(0);
+        trashVO.setReport(false);
 
-        when(trashService.add(trashVO)).thenReturn(trashVO);
         TrashVO resultVo = trashService.add(trashVO);
 
         assertEquals(trashVO.getId(),resultVo.getId());
@@ -41,29 +48,88 @@ public class TrashServiceImplTest {
     }
 
     @Test
-    public void testUpdate() throws Exception {
-        /*
-        TrashVO trashVO = trashService.update("f6e9b574-30c5-439a-bd40-34026c4b7604", new TrashVO());
-        TrashVO trashVO1 = trashService.findById("f6e9b574-30c5-439a-bd40-34026c4b7604");
-        assertEquals(trashVO.getTitle(),trashVO1.getTitle());
-        */
+    public void testFindById() throws Exception {
+        TrashVO trashVO = new TrashVO();
+        trashVO.setId("123");
+        trashVO.setThumbnailUrl("");
+        trashVO.setCategories("Unused Goods");
+        trashVO.setTrash_condition("Good");
+        trashVO.setUsername("Yanuar");
+        trashVO.setTitle("Test Trash");
+        trashVO.setStatus(0);
+        trashVO.setDescription("Test Description");
+        trashVO.setDistance(10);
+        trashVO.setLatitude(20);
+        trashVO.setLongitude(20);
+        trashVO.setPhoto_url("");
+        trashVO.setSize(0);
+        trashVO.setReport(false);
+
+        TrashVO tempVo = trashService.add(trashVO);
+        TrashVO resultVO = trashService.findById(tempVo.getId());
+
+        assertEquals(resultVO.getId(),tempVo.getId());
     }
 
     @Test
-    public void testDelete() throws Exception {
+    public void testUpdate() throws Exception {
 
+        TrashVO trashVO = new TrashVO();
+        trashVO.setId("123");
+        trashVO.setThumbnailUrl("");
+        trashVO.setCategories("Unused Goods");
+        trashVO.setTrash_condition("Good");
+        trashVO.setUsername("Yanuar");
+        trashVO.setTitle("Test Trash");
+        trashVO.setStatus(0);
+        trashVO.setDescription("Test Description");
+        trashVO.setDistance(10);
+        trashVO.setLatitude(20);
+        trashVO.setLongitude(20);
+        trashVO.setPhoto_url("");
+        trashVO.setSize(0);
+        trashVO.setReport(false);
+
+        TrashVO tempVo = trashService.add(trashVO);
+
+        trashVO.setDescription("New Description");
+
+
+        TrashVO resultVO = trashService.update(tempVo.getId(), trashVO);
+
+        assertEquals(resultVO.getDescription(),"New Description");
     }
 
     @Test
     public void testFindAll() throws Exception {
-        /*
-        Collection<TrashVO> trashVOCollection= trashService.findAll();
-        assertEquals(trashVOCollection.size(), 7);
-        */
+
+        TrashVO trashVO = new TrashVO();
+        trashVO.setThumbnailUrl("");
+        trashVO.setCategories("Unused Goods");
+        trashVO.setTrash_condition("Good");
+        trashVO.setTitle("Test Trash");
+        trashVO.setStatus(0);
+        trashVO.setDistance(10);
+        trashVO.setLatitude(20);
+        trashVO.setLongitude(20);
+        trashVO.setPhoto_url("");
+        trashVO.setSize(0);
+        trashVO.setReport(false);
+        trashVO.setId("234");
+        trashVO.setUsername("Daniel");
+        trashVO.setDescription("New Description");
+
+        TrashVO tempVo = trashService.add(trashVO);
+
+        Collection<TrashVO> collection = trashService.findAll();
+
+        System.out.print(collection.toString());
+
+        assertEquals(2,collection.size());
     }
 
-    @Test
-    public void testFindById() throws Exception {
 
-    }
+
+
+
 }
